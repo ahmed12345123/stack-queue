f7d92796073260e99c6327f7514c52f3314398d4
ahmed12345123 <74160648+ahmed12345123@users.noreply.github.com>
2025-01-01 16:25:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -106,106 +106,106 @@ int main() {
 
 
 
-//#include <iostream>
-//using namespace std;
-//
-//class LinkedListQueue {
-//private:
-//    struct Node {
-//        int data;
-//        Node* next;
-//        Node(int val) : data(val), next(nullptr) {}
-//    };
-//    Node* front;
-//    Node* rear;
-//
-//public:
-//    LinkedListQueue() : front(nullptr), rear(nullptr) {}
-//
-//    void enQueue(int data) {
-//        Node* newNode = new Node(data);
-//        if (!rear) {
-//            front = rear = newNode;
-//            return;
-//        }
-//        rear->next = newNode;
-//        rear = newNode;
-//    }
-//
-//    int deQueue() {
-//        if (!front) {
-//            throw runtime_error("Queue is empty!");
-//        }
-//        int value = front->data;
-//        Node* temp = front;
-//        front = front->next;
-//        if (!front) {
-//            rear = nullptr;
-//        }
-//        delete temp;
-//        return value;
-//    }
-//
-//    int getFront() {
-//        if (!front) {
-//            throw runtime_error("Queue is empty!");
-//        }
-//        return front->data;
-//    }
-//
-//    int getRear() {
-//        if (!rear) {
-//            throw runtime_error("Queue is empty!");
-//        }
-//        return rear->data;
-//    }
-//
-//    void display() {
-//        Node* current = front;
-//        while (current) {
-//            cout << current->data << " ";
-//            current = current->next;
-//        }
-//        cout << endl;
-//    }
-//};
-//
-//
-//
-//
-//
-//int main() {
-//    LinkedListQueue queue;
-//
-//    // Enqueue some elements
-//    queue.enQueue(10);
-//    queue.enQueue(20);
-//    queue.enQueue(30);
-//
-//    cout << "Queue after enQueue operations: ";
-//    queue.display();
-//
-//    // Get the front and rear elements
-//    cout << "Front: " << queue.getFront() << endl;
-//    cout << "Rear: " << queue.getRear() << endl;
-//
-//    // Dequeue an element
-//    cout << "Dequeue: " << queue.deQueue() << endl;
-//
-//    // Display the queue after dequeue
-//    cout << "Queue after deQueue: ";
-//    queue.display();
-//
-//    // Dequeue remaining elements
-//    cout << "Dequeue: " << queue.deQueue() << endl;
-//    cout << "Dequeue: " << queue.deQueue() << endl;
-//
-//    // Try to dequeue from an empty queue
-//    try {
-//        cout << "Dequeue from empty queue: " << queue.deQueue() << endl;
-//    } catch (const runtime_error& e) {
-//        cout << "Error: " << e.what() << endl;
-//    }
-//
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+class LinkedListQueue {
+private:
+   struct Node {
+       int data;
+       Node* next;
+       Node(int val) : data(val), next(nullptr) {}
+   };
+   Node* front;
+   Node* rear;
+
+public:
+   LinkedListQueue() : front(nullptr), rear(nullptr) {}
+
+   void enQueue(int data) {
+       Node* newNode = new Node(data);
+       if (!rear) {
+           front = rear = newNode;
+           return;
+       }
+       rear->next = newNode;
+       rear = newNode;
+   }
+
+   int deQueue() {
+       if (!front) {
+           throw runtime_error("Queue is empty!");
+       }
+       int value = front->data;
+       Node* temp = front;
+       front = front->next;
+       if (!front) {
+           rear = nullptr;
+       }
+       delete temp;
+       return value;
+   }
+
+   int getFront() {
+       if (!front) {
+           throw runtime_error("Queue is empty!");
+       }
+       return front->data;
+   }
+
+   int getRear() {
+       if (!rear) {
+           throw runtime_error("Queue is empty!");
+       }
+       return rear->data;
+   }
+
+   void display() {
+       Node* current = front;
+       while (current) {
+           cout << current->data << " ";
+           current = current->next;
+       }
+       cout << endl;
+   }
+};
+
+
+
+
+
+int main() {
+   LinkedListQueue queue;
+
+   // Enqueue some elements
+   queue.enQueue(10);
+   queue.enQueue(20);
+   queue.enQueue(30);
+
+   cout << "Queue after enQueue operations: ";
+   queue.display();
+
+   // Get the front and rear elements
+   cout << "Front: " << queue.getFront() << endl;
+   cout << "Rear: " << queue.getRear() << endl;
+
+   // Dequeue an element
+   cout << "Dequeue: " << queue.deQueue() << endl;
+
+   // Display the queue after dequeue
+   cout << "Queue after deQueue: ";
+   queue.display();
+
+   // Dequeue remaining elements
+   cout << "Dequeue: " << queue.deQueue() << endl;
+   cout << "Dequeue: " << queue.deQueue() << endl;
+
+   // Try to dequeue from an empty queue
+   try {
+       cout << "Dequeue from empty queue: " << queue.deQueue() << endl;
+   } catch (const runtime_error& e) {
+       cout << "Error: " << e.what() << endl;
+   }
+
+   return 0;
+}
